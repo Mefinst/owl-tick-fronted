bc8b33617bf75c8bbcac3fd3d5ffa87191c099ff
mefinst <mefinst@yandex.ru>
2022-06-21 13:28:59 +0300

    MVP add login form component

---
+++ components/login-form.vue
@@ -0,0 +1,17 @@
+<style></style>
+
+<template>
+	<div class="login-form">
+		<label class="input-field">
+			<span>Username</span>
+			<input type="text" name="username" />
+		</label>
+
+		<label class="input-field">
+			<span>Pasword</span>
+			<input type="password" name="password" />
+		</label>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
