6ee6f503cc7ccabff662deef0bb389372c5195fd
Aleksei Yunev <mefinst@yandex.ru>
2022-10-17 13:27:36 +0000

    docker space

--- .space.kts
+++ .space.kts
@@ -20,24 +20,28 @@ job("Unit tests") {
 }
 
 job("Docker image") {
-    resources {
-        cpu = 1.cpu
-        memory = 1000.mb
-    }    
-    // get auth data from secrets and put it to env vars
-    env["DOCKER_USER"] = "token"
-    env["DOCKER_TOKEN"] = Secrets("DOCKER_REGISTRY_TOKEN")
-
-    // put auth data to Docker config
-    beforeBuildScript {
-        content = """
-                B64_AUTH=${'$'}(echo -n ${'$'}DOCKER_USER:${'$'}DOCKER_TOKEN | base64 -w 0)
-                echo "{\"auths\":{\"https://cr.selcloud.ru/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
-            """
-    }
+  
     docker {
+        resources {
+            cpu = 1.cpu
+            memory = 1000.mb
+        }    
+        
+        // get auth data from secrets and put it to env vars
+        env["DOCKER_USER"] = "token"
+        env["DOCKER_TOKEN"] = Secrets("DOCKER_REGISTRY_TOKEN")
+
+        // put auth data to Docker config
+        beforeBuildScript {
+            content = """
+                    B64_AUTH=${'$'}(echo -n ${'$'}DOCKER_USER:${'$'}DOCKER_TOKEN | base64 -w 0)
+                    echo "{\"auths\":{\"https://cr.selcloud.ru/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
+                """
+        }
+        
         build {
         }
+        
         push("cr.selcloud.ru/mefinst/owl-tick-frontend") {
         	tags("space-test")
         }
