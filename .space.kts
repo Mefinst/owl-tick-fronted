1b0ca901108c4b8125899bad3a6e03191a5d1057
Aleksei Yunev <mefinst@yandex.ru>
2022-10-17 11:21:57 +0000

    Add space job

---
+++ .space.kts
@@ -0,0 +1,15 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Unit tests") {
+    container(displayName = "Unit tests", image = "node:lts") {
+    	shellScript(interpreter = "/bin/sh"
+            content = """
+                npm ci
+                npm run test
+            """)
+    }
+}
